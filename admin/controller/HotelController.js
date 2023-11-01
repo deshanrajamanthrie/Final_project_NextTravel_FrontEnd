a57e7666c7b0ea36722703510a3496bbc6d4824f
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-11-02 01:41:06 +0530

    update content

--- admin/controller/HotelController.js
+++ admin/controller/HotelController.js
@@ -1,9 +1,31 @@
 import {Hotel} from "../model/Hotel.js";
+import {Contact} from "../model/Contact.js";
 
 export class HotelController {
     constructor() {
         this.loadAllHotels();
-        $("#btn-save-hotel").click(this.saveHotel.bind(this))
+        $("#btn-save-hotel").click(this.saveHotel.bind(this));
+        $("#btn-deleteGuide").click(this.deleteHotel.bind(this));
+    }
+
+    static url = "http://deshanz-vivobook:8080/api/v1/hotel/consume";
+
+    deleteHotel() {
+        let id = $("#txtHotel-id").val();
+        $.ajax({
+            url: HotelController.url + "?id=" + id,
+            method: "DELETE",
+            dataType: "json",
+            success: function (resp) {
+                if (resp.code === 200) {
+                    Swal.fire(resp.massage);
+                } else {
+                    Swal.fire(resp.massage);
+                }
+            }
+        });
+        this.clearTextField();
+        this.loadAllHotels();
     }
 
 
@@ -22,30 +44,41 @@ export class HotelController {
         let op3 = $("#txtHotelOp3").val();
         let op4 = $("#txtHotelOp4").val();
 
-        let contacts=[];
-        contacts[0]=[email,Number.parseInt(contactNum1),Number.parseInt(contactNum2)]
 
-        /*let contacts = [email, contactNum1, contactNum2];
-         let contacts[0]*/
+        let Hotel = {     //create Literal Base Object
+            id: id,
+            name: name,
+            starRate: Number.parseInt(starRate),
+            location: location,
+            contacts: [{
+                email: email,
+                contactNum1: contactNum1,
+                contactNum2: contactNum2
+            }
+            ],
+            pets: pets,
+            op1: op1,
+            op2: op2,
+            op3: op3,
+            op4: op4
 
-        let hotel = new Hotel(id, name,Number.parseInt(starRate),location, contacts,  pets, Number.parseInt(op1),Number.parseInt(op2),Number.parseInt(op3),Number.parseInt(op4));
-       console.log(hotel)
-        let postUrl = "http://deshanz-vivobook:8080/api/v1/hotel/consume";
+        }
+        // let postUrl = "http://deshanz-vivobook:8080/api/v1/hotel/consume";
         $.ajax({
-            url: postUrl,
+            url: HotelController.url,
             method: "POST",
             contentType: "application/json",
-            data: JSON.stringify(hotel),   //java Script obj convert To Json Object
+            data: JSON.stringify(Hotel),   //java Script obj convert To Json Object
             success: function (resp) {
-                if(resp.code===200){
-                   /* Swal.fire(resp.message);*/
-                    alert(resp.message);
-                }else {
-                    alert(resp.message);
+                if (resp.code === 200) {
+                    Swal.fire(resp.massage);
+                } else {
+                    Swal.fire(resp.massage);
                 }
             }
         });
-
+        this.clearTextField();
+        this.loadAllHotels();
 
     }
 
@@ -62,11 +95,57 @@ export class HotelController {
                 for (const hotel of resp.data) {
                     const hotelRow = `<tr><td>${hotel.id}</td><td>${hotel.name}</td><td>${hotel.starRate}</td> <td>${hotel.location}</td> <td>${hotel.contacts[0].email}</td> <td>${hotel.contacts[0].contactNum1}</td> <td>${hotel.contacts[0].contactNum2}</td><td>${hotel.pets}</td><td>${hotel.op1}</td><td>${hotel.op2}</td><td>${hotel.op3}</td><td>${hotel.op4}</td></tr>`;
                     $("#tbl-hotel-body").append(hotelRow);
+
+
+                    $("#tbl-hotel-body>:last-child").click(function () {
+                        let id = $(this).children().eq(0).text();
+                        let name = $(this).children().eq(1).text();
+                        let starRate = $(this).children().eq(2).text();
+                        let location = $(this).children().eq(3).text();
+                        let email = $(this).children().eq(4).text();
+                        let contactNum1 = $(this).children().eq(5).text();
+                        let contactNum2 = $(this).children().eq(6).text();
+                        let petsAllow = $(this).children().eq(7).text();
+                        let op1 = $(this).children().eq(8).text();
+                        let op2 = $(this).children().eq(9).text();
+                        let op3 = $(this).children().eq(10).text();
+                        let op4 = $(this).children().eq(11).text();
+
+
+                        $("#txtHotel-id").val(id);
+                        $("#txtHotel-name").val(name);
+                        $("#txtStarRate").val(starRate);
+                        $("#txtHotelLocation").val(location);
+                        $("#txtHotelEmail").val(email);
+                        $("#txtContactNum1").val(contactNum1);
+                        $("#txtContactNum2").val(contactNum2);
+                        $("#txtPetsAllow").val(petsAllow);
+                        $("#txtHotelOp1").val(op1);
+                        $("#txtHotelOp2").val(op2);
+                        $("#txtHotelOp3").val(op3);
+                        $("#txtHotelOp4").val(op4);
+                    });
+
+
                 }
             }
 
         });
+    }
 
+    clearTextField() {
+        $("#txtHotel-id").val("");
+        $("#txtHotel-name").val("");
+        $("#txtStarRate").val("");
+        $("#txtHotelLocation").val("");
+        $("#txtHotelEmail").val("");
+        $("#txtContactNum1").val("");
+        $("#txtContactNum2").val("");
+        $("#txtPetsAllow").val("");
+        $("#txtHotelOp1").val("");
+        $("#txtHotelOp2").val("");
+        $("#txtHotelOp3").val("");
+        $("#txtHotelOp4").val("");
 
     }
 
--- admin/model/Hotel.js
+++ admin/model/Hotel.js
@@ -1,95 +1,17 @@
 export class Hotel {
     constructor(id, name, starRate, location, contacts, pets, op1, op2, op3, op4) {
-        this._id = id;
-        this._name = name;
-        this._starRate = starRate;
-        this._location = location;
-        this._pets = pets;
-        this._op1 = op1;
-        this._op2 = op2;
-        this._op3 = op3;
-        this._op4 = op4;
-        this._contacts = contacts;
+        this.id = id;
+        this.name = name;
+        this.starRate = starRate;
+        this.location = location;
+        this.contacts = contacts;
+        this.pets = pets;
+        this.op1 = op1;
+        this.op2 = op2;
+        this.op3 = op3;
+        this.op4 = op4;
+
     }
 
 
-    get id() {
-        return this._id;
-    }
-
-    set id(value) {
-        this._id = value;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(value) {
-        this._name = value;
-    }
-
-    get starRate() {
-        return this._starRate;
-    }
-
-    set starRate(value) {
-        this._starRate = value;
-    }
-
-    get location() {
-        return this._location;
-    }
-
-    set location(value) {
-        this._location = value;
-    }
-
-    get contacts() {
-        return this._contacts;
-    }
-
-    set contacts(value) {
-        this._contacts = value;
-    }
-
-    get pets() {
-        return this._pets;
-    }
-
-    set pets(value) {
-        this._pets = value;
-    }
-
-    get op1() {
-        return this._op1;
-    }
-
-    set op1(value) {
-        this._op1 = value;
-    }
-
-    get op2() {
-        return this._op2;
-    }
-
-    set op2(value) {
-        this._op2 = value;
-    }
-
-    get op3() {
-        return this._op3;
-    }
-
-    set op3(value) {
-        this._op3 = value;
-    }
-
-    get op4() {
-        return this._op4;
-    }
-
-    set op4(value) {
-        this._op4 = value;
-    }
 }
